7cb9febd123ffc8890b590e876e2836233e384e3
Federico Serron <fede20041@gmail.com>
2025-02-04 10:22:50 +1100

    Changed images for ASCII icons

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  let palo = ["Corazones", "Picas", "Treboles", "Diamantes"];
+  let palo = ["♦", "♥", "♠", "♣"];
   let number = [
     "A",
     "2",
@@ -29,29 +29,51 @@ window.onload = function() {
     let randomNumber = number[Math.floor(Math.random() * number.length)];
 
     let cardNumber = document.querySelector("#number");
-    let cardImgPalo = document.querySelectorAll(".palo");
+    let cardsPalos = document.querySelectorAll(".palo");
 
-    let imgPalo = randomPalo => {
-      switch (randomPalo) {
-        case "Corazones":
-          return "https://static.vecteezy.com/system/resources/previews/025/088/110/non_2x/illustration-of-heart-playing-card-symbol-vector.jpg";
+    // let imgPalo = randomPalo => {
+    //   switch (randomPalo) {
+    //     case "Corazones":
+    //       return "https://static.vecteezy.com/system/resources/previews/025/088/110/non_2x/illustration-of-heart-playing-card-symbol-vector.jpg";
 
-        case "Picas":
-          return "https://i.pinimg.com/1200x/7c/f8/0f/7cf80f36bdc3ad01b78d0a49e8533027.jpg";
+    //     case "Picas":
+    //       return "https://i.pinimg.com/1200x/7c/f8/0f/7cf80f36bdc3ad01b78d0a49e8533027.jpg";
 
-        case "Treboles":
-          return "https://upload.wikimedia.org/wikipedia/commons/8/8a/SuitClubs.svg";
+    //     case "Treboles":
+    //       return "https://upload.wikimedia.org/wikipedia/commons/8/8a/SuitClubs.svg";
 
-        case "Diamantes":
-          return "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/SuitDiamonds.svg/640px-SuitDiamonds.svg.png";
-      }
-    };
+    //     case "Diamantes":
+    //       return "https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/SuitDiamonds.svg/640px-SuitDiamonds.svg.png";
+    //   }
+    // };
 
-    cardImgPalo.forEach(element => {
-      element.src = imgPalo(randomPalo);
-    });
+    // cardImgPalo.forEach(element => {
+    //   element.src = imgPalo(randomPalo);
+    // });
 
     cardNumber.innerHTML = randomNumber;
+
+    cardsPalos.forEach(palo => {
+      switch (randomPalo) {
+        case "♥":
+          palo.className = "heart palo";
+          break;
+
+        case "♦":
+          palo.className = "diamond palo";
+          break;
+
+        case "♠":
+          palo.className = "spade palo";
+          break;
+
+        case "♣":
+          palo.className = "club palo";
+          break;
+      }
+
+      palo.innerHTML = randomPalo;
+    });
   }
 
   document
